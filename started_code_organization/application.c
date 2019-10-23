340d7d8f7af52ae35d54e6aa8ec945520276fcb4
Vitor Hugo <vhlg99@gmail.com>
2019-10-23 01:10:00 +0100

    started application

---
+++ started_code_organization/application.c
@@ -0,0 +1,109 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <signal.h>
+
+#include "flags.h"
+
+//a função main será exclusiva e estará presente neste mesmo ficheiro
+int main(int argc, char** argv){
+
+
+    struct termios oldtio,newtio; 
+    int fd = open(argv[1], O_RDWR | O_NOCTTY ); //abertura de um file descriptor associado a um ficheiro passado como argumento na consola
+
+    if (fd <0) {
+        printf("Error opening FILE DESCRIPTOR\n");
+        exit(-1);
+    }
+
+    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+        perror("tcgetattr");
+        exit(-1);
+    }
+
+
+    bzero(&newtio, sizeof(newtio));
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    newtio.c_lflag = 0;
+
+    newtio.c_cc[VTIME]    = 2;   /* inter-character timer unused */
+    newtio.c_cc[VMIN]     = 0;   /* blocking read until 5 chars received */
+
+
+
+    tcflush(fd, TCIOFLUSH);
+
+    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+
+/*
+*
+*  ESTABELICIMENTO DA COMUNICAÇÃO 
+*   
+*   declarar porta de série a usar, 0,1 ou 2
+*   Pode ser do tipo WRITER or READER
+*   Se for writer tem de obrigatoriamente ter um diretório do ficheiro se for reader nao
+*
+*/
+
+FILE *file; //associado ao ficheiro que vai ser aberto para transmitir de um computador para outro
+int comunication_type;
+
+//processar argumentos
+    if (((strcmp("/dev/ttyS0", argv[1])!=0) && (strcmp("/dev/ttyS1", argv[1])!=0) && (strcmp("/dev/ttyS2", argv[1])!=0))){
+	    
+        printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+	    exit(1);
+	}
+
+
+    if(argv[2] == "sender"){
+        comunication_type = TRANSMITTER;
+        if(argc != 4){
+            printf("Must be specified a file path to transmit\n");
+            exit(1);
+        }
+        else{
+            file=fopen(argv[3],"rb"); //read file in binary mode
+            if(file == NULL){
+                printf("unable to open file\n");
+                exit(1);
+            }
+        }
+    }
+    else if(argv[2] == "receiver"){
+        comunication_type = RECEIVER;
+    }
+    else{
+        printf("argv[2] must be sender or receiver");
+        exit(1);
+    }
+
+/* llopen para estabelecer ligaçoes */
+
+
+
+
+
+
+/*----------------------------------------------------------------------------------------------------------------------------------------------*/
+
+     if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    close(fd);
+    return 0;
+}
---
+++ started_code_organization/flags.h
@@ -0,0 +1,25 @@
+#define BAUDRATE B9600
+#define MODEMDEVICE "/dev/ttyS0"
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
+#define M_FLAG 0x7e
+#define M_A_SND 0x01
+#define M_A_REC 0x03
+#define M_C_SND 0x07
+#define M_C_REC 0x03
+#define DISC 0x0B
+#define TRANSMITTER 0
+#define RECEIVER 1
+#define MAX_SIZE 255
+#define ESCAPE 0x7d
+#define ESCAPE_FLAG 0x5d
+#define FLAG_ESC 0x5e
+#define MAX_TRIES 3
+#define ACCEPTED 1		//ACK sent correctly
+#define REJECTED -1		//ACK wrong
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define RR0 0x05
+#define RR1 0x85
+#define REJ0 0x01
+#define REJ1 0x81
