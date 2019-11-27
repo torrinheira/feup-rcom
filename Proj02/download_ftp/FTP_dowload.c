5665942a656a0618c2621153e0ffa20ca6238460
Vitor Hugo <vhlg99@gmail.com>
2019-11-27 16:58:23 +0000

    arguments extraction completed

--- Proj02/download_ftp/FTP_dowload.c
+++ Proj02/download_ftp/FTP_dowload.c
@@ -4,7 +4,8 @@
 int main(int argc, char ** argv){
 
     if(argc != 2){
-        perror("Arguments are not correct");   
+        printf("Arguments are not correct\n"); 
+        return -2;  
     }
    
 
@@ -12,15 +13,15 @@ int main(int argc, char ** argv){
 
     int url_size = strlen(argv[1]);
     int new_size = url_size - 7;
-    int name_size = 0;
-    int password_size = 0;
-    int host_size = 0;
-    int url_path_size = 0;
+
+    
     
     if(new_size <= 8){
-        perror("Error: Check url... Something went wrong");
+        printf("Error: Check url... Something went wrong\n");
+        return -1;
     }
 
+    //obtem nome do utilizador
     char argv1[new_size];
 
     int j = 7;
@@ -29,25 +30,84 @@ int main(int argc, char ** argv){
         j++;
     }
 
-    //argv1 é uma string igual à passada no argv1 mas sem a parte do ftp://
+    //state machine
 
-    //obtem nome do utilizador
-    while(argv1[name_size] != ':'){
-        name_size++;
-    }
+    char name[100];
+    memset(name, 0, 100);
 
-    char name[name_size];
+    char password[100];
+    memset(password, 0, 100);
 
-    for(size_t i = 0 ; i < name_size ; i++){
-        name[i] = argv1[i];
-    }
+    char host[100];
+    memset(host, 0, 100);
 
-    //obter password do utilizador
+    char path_file[100];
+    memset(path_file, 0, 100);
+
+    int estado = 0;
+    int i = 0;
+    int indice = 0;
     
-    printf("%s\n", name);
-    printf("%s\n",argv1);
+    while( i < strlen(argv1)){
+        switch (estado)
+        {
+        case 0:
+            if(argv1[i] == ':'){
+                estado = 1;
+                indice = 0;
+            }
+            else{
+                name[indice] = argv1[i];
+                indice++;
+            }
+            break;
+        
+        case 1:
+            if(argv1[i] == '@'){
+                estado = 2;
+                indice = 0;
+            }
+            else{
+                password[indice] = argv1[i];
+                indice++;
+            }
+            break;
+        case 2:
+            if(argv1[i] == ']'){
+                estado = 3;
+                indice = 0;
+            }
+            else{
+                printf("Error parsing URL...\n");
+            }
+            break;
+        case 3:
+            if(argv1[i] == '/'){
+                estado = 4;
+                indice = 0;
+            }
+            else{
+                host[indice] = argv1[i];
+                indice++;
+            }
+            break;
+        
+        case 4:
+            path_file[indice] = argv1[i];
+            indice++;
+            break;
+        }
 
-    //=============================================================================================//
+        i++;
+    }
+
+
+    printf("%s\n", name);
+    printf("%s\n", password);
+    printf("%s\n", host);
+    printf("%s\n",path_file);
+
     
     return 0;
+    
 }
